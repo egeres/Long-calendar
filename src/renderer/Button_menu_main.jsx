2cc8385f5d531ede711862f57d95602071da2bb6
Mee <rafaleto@gmail.com>
2023-04-08 13:45:38 +0200

    feat: Removed eva icons

--- src/renderer/Button_menu_main.jsx
+++ src/renderer/Button_menu_main.jsx
@@ -13,7 +13,14 @@ export default class Button_menu_main extends Component
     render()
     {
         return <div id="button_menu" onClick={() => this.props.onClick()} >
-            <i data-eva="menu-outline" data-eva-fill="#FFF" />
+            {/* <i data-eva="menu-outline" data-eva-fill="#FFF" /> */}
+
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M4 6l16 0"></path>
+            <path d="M4 12l16 0"></path>
+            <path d="M4 18l16 0"></path>
+            </svg>
         </div>
     }
 }
